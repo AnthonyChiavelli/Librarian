4e4bd43470a838d33ac0f69c70bdc2c44cd618fe
Anthony Reid <anthonyreid99@gmail.com>
2013-06-26 11:26:16 -0400

    Now using GLADE gui builder

--- main.py
+++ main.py
@@ -1,46 +1,15 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
-import pygtk
-pygtk.require('2.0')
-import gtk
+from gi.repository import Gtk
 
-class App:
+class App():
+
   def __init__(self):
-    #Create a new window for application
-    self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-    #Connect events to event handler functions
-    self.window.connect("delete_event", self.delete_event)
-    self.window.connect("destroy", self.destroy)
-
-    #Test button
-    self.button = gtk.Button("Test")
-    self.button.connect("clicked", self.clicked, None)
-    self.window.add(self.button)
-    self.button.show()
-
-    #Show window
-    self.window.show()
-
-  #Callback function connected to clicking of button
-  def clicked(self, widget, data=None):
-    print "Clicked"
-
-  #Callback function connected to titlebar close 
-  def delete_event(self):
-    return False #True to remain open, false to emit destroy signal
-
-  #Callback function connected to gtk_widget_destroy() call
-  #or False returned from self.delete_event()
-  def destroy(self, widget, data=None):
-    gtk.main_quit()
-
-  def main(self):
-    gtk.main()
+    self.builder = Gtk.Builder()
+    self.builder.add_from_file("this3.glade")
+    self.window = self.builder.get_object('window1')
+    self.window.show_all()
 
 
-#If invoked directly
-if __name__ == "__main__":
-  app = App()
-  app.main()
-  print "Librarian"
-
+app = App()
+Gtk.main()
