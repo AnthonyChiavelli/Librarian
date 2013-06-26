eb225039a77b9b277fb9fa218304f30f80fd8e48
Anthony Reid <anthonyreid99@gmail.com>
2013-06-26 16:11:03 -0400

    Added test demo

--- id3_process.py
+++ id3_process.py
@@ -1,3 +1,15 @@
-import mutagen
+from mutagen.id3 import ID3, TIT2
+import os
 
-print hello
+def process_files(directory):
+  #Recursively iterator over files
+  for _, _,files in os.walk(directory):
+    for f in files:
+      #Construct complete path
+      f = directory + "/" + f
+      #Update metadata
+      metadata = ID3(f)
+      metadata.add(TIT2(encoding=3, text=u"Rick Astley"))
+      metadata.save()
+
+      
