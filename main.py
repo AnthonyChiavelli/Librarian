91f3393c3212bd8612bf1faac614425f652a90d9
Anthony Reid <anthonyreid99@gmail.com>
2013-06-26 13:40:26 -0400

    Callback for toggling id3 widgets started

--- main.py
+++ main.py
@@ -5,11 +5,33 @@ from gi.repository import Gtk
 class App():
 
   def __init__(self):
+    
+    #Load layout from XML
     self.builder = Gtk.Builder()
-    self.builder.add_from_file("this3.glade")
-    self.window = self.builder.get_object('window1')
+    self.builder.add_from_file("gui/main_layout.glade")
+    self.window = self.builder.get_object('top_window')
+
+    #Connect close event to Gtk.main_quit
+    self.window.connect("delete-event", Gtk.main_quit)
+    
+    #Map glade handler names to functions
+    self.handlers = {"id3_toggled": self.toggle_id3}
+    self.builder.connect_signals(self.handlers)
+    
+    #Show
     self.window.show_all()
+    
+
+  #Callback for toggle event on id3_checkbox
+  #Toggles sensitivity of id3-related widgets
+  def toggle_id3(self, widget):
+    #Toggle sensitivity 
+    state = not widget.get_sensitive()
+    self.builder.get_object('framecode_entry').set_sensitive(state)
+    
 
 
-app = App()
-Gtk.main()
+#If invoked directly
+if __name__ == "__main__":
+  app = App()
+  Gtk.main()
