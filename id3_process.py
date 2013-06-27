8fcc5fe99fd7b58a09fc58937213b576e7bff6b8
Anthony Reid <anthonyreid99@gmail.com>
2013-06-26 20:44:00 -0400

    Added call to API to retrieve artist and song title

--- id3_process.py
+++ id3_process.py
@@ -1,15 +1,22 @@
-from mutagen.id3 import ID3, TIT2
+from mutagen.id3 import ID3, TIT2, TPE1
 import os
+import identify
 
 def process_files(directory):
   #Recursively iterator over files
   for _, _,files in os.walk(directory):
     for f in files:
-      #Construct complete path
+      if (not "mp3" in f): continue
+      #Get correct metadata
       f = directory + "/" + f
+      correct_metadata = identify.id_song(f)
       #Update metadata
-      metadata = ID3(f)
-      metadata.add(TIT2(encoding=3, text=u"Rick Astley"))
-      metadata.save()
+      old_metadata = ID3()
+      old_metadata.add(TIT2(encoding=3, text=correct_metadata["title"]))
+      old_metadata.add(TPE1(encoding=3, text=correct_metadata["artist"]))
+      old_metadata.save(f)
+      
+
+
 
       
